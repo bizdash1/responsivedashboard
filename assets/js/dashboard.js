c6ce2a1c517338ca583fa4744df6ca0b7c7a7bfe
bizdash1 <67599053+bizdash1@users.noreply.github.com>
2020-11-19 16:27:15 -0600

    Add files via upload
    full update to dashboard design and required supporting items

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -29,8 +29,8 @@ $(function () {
   });
 
 
-  // Start chart
-  var chart = document.getElementById('myChart');
+  // Start monthly chart
+  var chart = document.getElementById('payPal12m');
   Chart.defaults.global.animation.duration = 2000; // Animation duration
   Chart.defaults.global.title.display = false; // Remove title
   Chart.defaults.global.title.text = "Chart"; // Title
@@ -46,41 +46,31 @@ $(function () {
   Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.02)';
   Chart.defaults.global.legend.display = false;
 
-  var myChart = new Chart(chart, {
+  var payPal12m = new Chart(chart, {
     type: 'bar',
     data: {
-      labels: ["January", "February", "March", "April", "May", 'Jul'],
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       datasets: [{
         label: "Lost",
         fill: false,
         lineTension: 0,
         data: [45, 25, 40, 20, 45, 20],
+        backgroundColor: "#4bc0c0",
         pointBorderColor: "#4bc0c0",
         borderColor: '#4bc0c0',
         borderWidth: 2,
         showLine: true,
-      }, {
-        label: "Succes",
-        fill: false,
-        lineTension: 0,
-        startAngle: 2,
-        data: [20, 40, 20, 45, 25, 60],
-        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-        backgroundColor: "transparent",
-        pointBorderColor: "#ff6384",
-        borderColor: '#ff6384',
-        borderWidth: 2,
-        showLine: true,
-      }]
+      }, 
+    ]
     },
   });
 
   //  Chart ( 2 )
-  var Chart2 = document.getElementById('myChart2').getContext('2d');
+  var Chart2 = document.getElementById('payPal12mandStripe12m').getContext('2d');
   var chart = new Chart(Chart2, {
     type: 'line',
     data: {
-      labels: ["January", "February", "March", "April", 'test', 'test', 'test', 'test'],
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       datasets: [{
         label: "My First dataset",
         backgroundColor: 'rgb(255, 99, 132)',
@@ -102,18 +92,6 @@ $(function () {
         borderColor: '#4bc0c0',
         borderWidth: 2,
         showLine: true,
-      }, {
-        label: "Month",
-        fill: false,
-        lineTension: .4,
-        startAngle: 2,
-        data: [40, 20, 5, 10, 30, 15, 15, 10],
-        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
-        backgroundColor: "transparent",
-        pointBorderColor: "#ffcd56",
-        borderColor: '#ffcd56',
-        borderWidth: 2,
-        showLine: true,
       }]
     },
 
@@ -125,56 +103,121 @@ $(function () {
     }
   });
 
-  var chart = document.getElementById('chart3');
-  var myChart = new Chart(chart, {
-    type: 'line',
+  var chart = document.getElementById('stripe12m');
+  var myChart2 = new Chart(chart, {
+    type: 'bar',
     data: {
-      labels: ["One", "Two", "Three", "Four", "Five", 'Six', "Seven", "Eight"],
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       datasets: [{
-        label: "Lost",
+        label: "Succes",
         fill: false,
-        lineTension: .5,
-        pointBorderColor: "transparent",
-        pointColor: "white",
-        borderColor: '#d9534f',
-        borderWidth: 0,
+        lineTension: 0,
+        startAngle: 2,
+        data: [20, 40, 20, 45, 25, 60],
+        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+        backgroundColor: "#ff6384",
+        pointBorderColor: "#ff6384",
+        borderColor: '#ff6384',
+        borderWidth: 2,
         showLine: true,
-        data: [0, 40, 10, 30, 10, 20, 15, 20],
-        pointBackgroundColor: 'transparent',
-      },{
-        label: "Lost",
-        fill: false,
-        lineTension: .5,
-        pointColor: "white",
-        borderColor: '#5cb85c',
-        borderWidth: 0,
-        showLine: true,
-        data: [40, 0, 20, 10, 25, 15, 30, 0],
-        pointBackgroundColor: 'transparent',
-      },
-                 {
-                   label: "Lost",
-                   fill: false,
-                   lineTension: .5,
-                   pointColor: "white",
-                   borderColor: '#f0ad4e',
-                   borderWidth: 0,
-                   showLine: true,
-                   data: [10, 40, 20, 5, 35, 15, 35, 0],
-                   pointBackgroundColor: 'transparent',
-                 },
-                 {
-                   label: "Lost",
-                   fill: false,
-                   lineTension: .5,
-                   pointColor: "white",
-                   borderColor: '#337ab7',
-                   borderWidth: 0,
-                   showLine: true,
-                   data: [0, 30, 10, 25, 10, 40, 20, 0],
-                   pointBackgroundColor: 'transparent',
-                 }]
+      },]
     },
   });
 
+    // Start daily chart
+    var chart = document.getElementById('payPal15d');
+    Chart.defaults.global.animation.duration = 2000; // Animation duration
+    Chart.defaults.global.title.display = false; // Remove title
+    Chart.defaults.global.title.text = "Chart"; // Title
+    Chart.defaults.global.title.position = 'bottom'; // Title position
+    Chart.defaults.global.defaultFontColor = '#999'; // Font color
+    Chart.defaults.global.defaultFontSize = 10; // Font size for every label
+  
+    // Chart.defaults.global.tooltips.backgroundColor = '#FFF'; // Tooltips background color
+    Chart.defaults.global.tooltips.borderColor = 'white'; // Tooltips border color
+    Chart.defaults.global.legend.labels.padding = 0;
+    Chart.defaults.scale.ticks.beginAtZero = true;
+    Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.1)';
+    Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.02)';
+    Chart.defaults.global.legend.display = false;
+  
+    var payPal15d = new Chart(chart, {
+      type: 'bar',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+        datasets: [{
+          label: "Lost",
+          fill: false,
+          lineTension: 0,
+          data: [45, 25, 40, 20, 45, 20],
+          backgroundColor: "#4bc0c0",
+          pointBorderColor: "#4bc0c0",
+          borderColor: '#4bc0c0',
+          borderWidth: 2,
+          showLine: true,
+        }, 
+      ]
+      },
+    });
+  
+    //  Chart ( 2 )
+    var Chart2 = document.getElementById('payPal15dandStripe15d').getContext('2d');
+    var chart = new Chart(Chart2, {
+      type: 'line',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+        datasets: [{
+          label: "My First dataset",
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 79, 116)',
+          borderWidth: 2,
+          pointBorderColor: false,
+          data: [5, 10, 5, 8, 20, 30, 20, 10],
+          fill: false,
+          lineTension: .4,
+        }, {
+          label: "Month",
+          fill: false,
+          lineTension: .4,
+          startAngle: 2,
+          data: [20, 14, 20, 25, 10, 15, 25, 10],
+          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+          backgroundColor: "transparent",
+          pointBorderColor: "#4bc0c0",
+          borderColor: '#4bc0c0',
+          borderWidth: 2,
+          showLine: true,
+        }]
+      },
+  
+      // Configuration options
+      options: {
+        title: {
+          display: false
+        }
+      }
+    });
+  
+    var chart = document.getElementById('stripe15d');
+    var myChart2 = new Chart(chart, {
+      type: 'bar',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
+        datasets: [{
+          label: "Succes",
+          fill: false,
+          lineTension: 0,
+          startAngle: 2,
+          data: [20, 40, 20, 45, 25, 60],
+          // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
+          backgroundColor: "#ff6384",
+          pointBorderColor: "#ff6384",
+          borderColor: '#ff6384',
+          borderWidth: 2,
+          showLine: true,
+        },]
+      },
+    });
+  
 });
+
